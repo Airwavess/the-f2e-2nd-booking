0bac9a03b2b0dca6e31a0c5a0f9eed6c3629f927
Airwaves <prairie5270@gmail.com>
2019-11-23 19:51:12 +0800

    Updates

---
+++ precache-manifest.ea9bea8b62376d53aff7711deb467237.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2205f1a21e24560a7fcdb5becb4e524",
+    "url": "/the-f2e-2nd-booking/index.html"
+  },
+  {
+    "revision": "181c50aa54bc45fa054c",
+    "url": "/the-f2e-2nd-booking/static/css/2.30861e7f.chunk.css"
+  },
+  {
+    "revision": "c6cfe8e22bcaa4a098a7",
+    "url": "/the-f2e-2nd-booking/static/css/main.61f82772.chunk.css"
+  },
+  {
+    "revision": "181c50aa54bc45fa054c",
+    "url": "/the-f2e-2nd-booking/static/js/2.e6329474.chunk.js"
+  },
+  {
+    "revision": "c6cfe8e22bcaa4a098a7",
+    "url": "/the-f2e-2nd-booking/static/js/main.68a531f3.chunk.js"
+  },
+  {
+    "revision": "313675d12cec41a23277",
+    "url": "/the-f2e-2nd-booking/static/js/runtime-main.fcabcc40.js"
+  },
+  {
+    "revision": "92b2b3373bd0df5b0adc0bc18e2fe7d2",
+    "url": "/the-f2e-2nd-booking/static/media/ArrowLeft.92b2b337.svg"
+  },
+  {
+    "revision": "c8c37a26d81e97cb82c1b99e9987ee84",
+    "url": "/the-f2e-2nd-booking/static/media/ArrowRight.c8c37a26.svg"
+  },
+  {
+    "revision": "7318a054a66fe79023232847e151b60e",
+    "url": "/the-f2e-2nd-booking/static/media/Breakfast icon.7318a054.svg"
+  },
+  {
+    "revision": "686b488067c2155bc463dfe51dc1ddbe",
+    "url": "/the-f2e-2nd-booking/static/media/Component.686b4880.svg"
+  },
+  {
+    "revision": "e3deccae183e2305a31c9f027c8efd3d",
+    "url": "/the-f2e-2nd-booking/static/media/Conditioner icon.e3deccae.svg"
+  },
+  {
+    "revision": "ca7f624927c523db69a394e8296477c3",
+    "url": "/the-f2e-2nd-booking/static/media/Confirm icon.ca7f6249.svg"
+  },
+  {
+    "revision": "ed5e5c95fce40d9de527e2d1b8263010",
+    "url": "/the-f2e-2nd-booking/static/media/Couch icon.ed5e5c95.svg"
+  },
+  {
+    "revision": "38da24eb51397c90de5c2af8be2bcd93",
+    "url": "/the-f2e-2nd-booking/static/media/Kid-friendly icon.38da24eb.svg"
+  },
+  {
+    "revision": "e8977e51f1bceec663b13832bf69ba59",
+    "url": "/the-f2e-2nd-booking/static/media/Mail.e8977e51.svg"
+  },
+  {
+    "revision": "2d424aa33fd2ce90d857b2bb52797ae8",
+    "url": "/the-f2e-2nd-booking/static/media/Mini bar icon.2d424aa3.svg"
+  },
+  {
+    "revision": "69a8b062fb97af3177ca7c70cda8e6ed",
+    "url": "/the-f2e-2nd-booking/static/media/No smoking icon.69a8b062.svg"
+  },
+  {
+    "revision": "5f4d70306b98b077a8e420199c7ccbb9",
+    "url": "/the-f2e-2nd-booking/static/media/Pet-friendly icon.5f4d7030.svg"
+  },
+  {
+    "revision": "6df839a512d6bb4cad00dd89f6f3684f",
+    "url": "/the-f2e-2nd-booking/static/media/Refrigerator icon.6df839a5.svg"
+  },
+  {
+    "revision": "7a838166b2660ba5ba0b3e36ead04873",
+    "url": "/the-f2e-2nd-booking/static/media/Room service icon.7a838166.svg"
+  },
+  {
+    "revision": "df74e9610dd13fa9632519c2225012eb",
+    "url": "/the-f2e-2nd-booking/static/media/Telephone icon.df74e961.svg"
+  },
+  {
+    "revision": "06ce307b1d2603f5f00d61a93d18c0fe",
+    "url": "/the-f2e-2nd-booking/static/media/Telephone.06ce307b.svg"
+  },
+  {
+    "revision": "7258a637e4848d781d93d801d127441b",
+    "url": "/the-f2e-2nd-booking/static/media/View icon.7258a637.svg"
+  },
+  {
+    "revision": "56d796464752741c524d70b1d845ad13",
+    "url": "/the-f2e-2nd-booking/static/media/Wifi icon.56d79646.svg"
+  }
+]);
